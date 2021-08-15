6c2b3cc80ec0dd1cb5e071ee5836c4ddd0741a26
huduzhu <88719966+huduzhu@users.noreply.github.com>
2021-08-15 16:13:19 +1000

    Update vminstall.sh

--- vminstall.sh
+++ vminstall.sh
@@ -14,6 +14,6 @@ tar xf /mnt/VMwareTools*.tar.gz -C /root
 perl /root/vmware-tools-distrib/vmware-install.pl
 pacman -S virtualbox-guest-utils xf86-video-vmware
 sudo pacman -S gnome-tweaks
-sudo pacman -S gnome
+sudo pacman -S gnome --ignore gnome-backgrounds gnome-books gnome-clocks gnome-color-manager gnome-contacts gnome-disk-utility gnome-documents gnome-maps gnome-music gnome-photos gnome-software gnome-themes-extra gnome-weather orca totem yelp
 sudo systemctl enable gdm
 pacman -S adobe-source-han-sans-cn-fonts adobe-source-han-sans-tw-fonts adobe-source-han-sans-jp-fonts adobe-source-han-sans-kr-fonts

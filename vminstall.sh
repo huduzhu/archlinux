2bcd157b4692c9169b9a3b2760ac12151afb91cc
huduzhu <88719966+huduzhu@users.noreply.github.com>
2021-08-15 15:25:44 +1000

    Update vminstall.sh

--- vminstall.sh
+++ vminstall.sh
@@ -6,7 +6,7 @@ pacman -S intel-ucode
 pacman -S xorg-server
 pacman -S mesa
 curl -O https://raw.githubusercontent.com/huduzhu/archlinux/main/vmautostart
-nano vmautostart
+nano -F vmautostart
 for x in {0..6}; do mkdir -p /etc/init.d/rc${x}.d; done
 mount /dev/cdrom /mnt
 tar xf /mnt/VMwareTools*.tar.gz -C /root

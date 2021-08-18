6d7bfaecd6e112bdca77c681426271c5082a1ae9
huduzhu <88719966+huduzhu@users.noreply.github.com>
2021-08-18 18:54:36 +1000

    Update vmxcfe.sh

--- vmxcfe.sh
+++ vmxcfe.sh
@@ -1,1 +1,11 @@
+#! /usr/bin/bash
 
+timedatectl set-timezone Australia/Victoria
+systemctl enable systemd-timesyncd
+pacman -S intel-ucode
+pacman -S xorg-server
+pacman -S mesa
+pacman -S open-vm-tools
+sudo pacman -S xcfe4 lightdm lightdm-gtk-greeter
+sudo systemctl enable lightdm-gtk-greeter
+pacman -S adobe-source-han-sans-cn-fonts adobe-source-han-sans-tw-fonts adobe-source-han-sans-jp-fonts adobe-source-han-sans-kr-fonts

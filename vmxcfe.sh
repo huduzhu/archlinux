6526e9fb4660343a8b71e630fadb577a4f9204cb
huduzhu <88719966+huduzhu@users.noreply.github.com>
2021-08-18 19:01:08 +1000

    Update vmxcfe.sh

--- vmxcfe.sh
+++ vmxcfe.sh
@@ -6,7 +6,7 @@ pacman -S intel-ucode
 pacman -S xorg-server
 pacman -S mesa
 pacman -S open-vm-tools
-sudo pacman -S xcfe4 lightdm 
+sudo pacman -S xfce4 lightdm 
 sudo pacman -S lightdm-gtk-greeter gvfs-smb
 sudo systemctl enable lightdm-gtk-greeter
 pacman -S adobe-source-han-sans-cn-fonts adobe-source-han-sans-tw-fonts adobe-source-han-sans-jp-fonts adobe-source-han-sans-kr-fonts

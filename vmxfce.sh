ad93384d1ae9667d04ea829638e3a4319914d38f
huduzhu <88719966+huduzhu@users.noreply.github.com>
2021-08-18 19:06:35 +1000

    Update vmxfce.sh

--- vmxfce.sh
+++ vmxfce.sh
@@ -2,6 +2,8 @@
 
 timedatectl set-timezone Australia/Victoria
 systemctl enable systemd-timesyncd
-pacman -S intel-ucode xorg-server mesa open-vm-tools xfce4 lightdm lightdm-gtk-greeter gvfs-smb
+pacman -S intel-ucode xorg-server mesa
+pacman -S open-vm-tools
+pacman -S xfce4 lightdm lightdm-gtk-greeter gvfs-smb
 sudo systemctl enable lightdm-gtk-greeter
 pacman -S adobe-source-han-sans-cn-fonts adobe-source-han-sans-tw-fonts adobe-source-han-sans-jp-fonts adobe-source-han-sans-kr-fonts

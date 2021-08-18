6dc596393711213269379db94ee0fe62c6decca5
huduzhu <88719966+huduzhu@users.noreply.github.com>
2021-08-18 19:08:49 +1000

    Update vmxfce.sh

--- vmxfce.sh
+++ vmxfce.sh
@@ -5,5 +5,5 @@ systemctl enable systemd-timesyncd
 pacman -S intel-ucode xorg-server mesa
 pacman -S open-vm-tools
 pacman -S xfce4 lightdm lightdm-gtk-greeter gvfs-smb
-sudo systemctl enable lightdm-gtk-greeter
+sudo systemctl enable lightdm
 pacman -S adobe-source-han-sans-cn-fonts adobe-source-han-sans-tw-fonts adobe-source-han-sans-jp-fonts adobe-source-han-sans-kr-fonts

743d526837979e41992cad4aecbb732c4c71cb1c
huduzhu <88719966+huduzhu@users.noreply.github.com>
2021-08-18 19:12:08 +1000

    Update vmxfce.sh

--- vmxfce.sh
+++ vmxfce.sh
@@ -3,7 +3,15 @@
 timedatectl set-timezone Australia/Victoria
 systemctl enable systemd-timesyncd
 pacman -S intel-ucode xorg-server mesa
-pacman -S open-vm-tools
+curl -O https://raw.githubusercontent.com/huduzhu/archlinux/main/vmwaretools.service
+nano vmwaretools.service
+for x in {0..6}; do mkdir -p /etc/init.d/rc${x}.d; done
+mv vmwaretools.service /etc/systemd/system/vmwaretools.service
+mount /dev/cdrom /mnt
+tar xf /mnt/VMwareTools*.tar.gz -C /root
+perl /root/vmware-tools-distrib/vmware-install.pl
+pacman -S virtualbox-guest-utils xf86-video-vmware
+systemctl enable vmwaretools.service  
 pacman -S xfce4 lightdm lightdm-gtk-greeter gvfs-smb
 sudo systemctl enable lightdm
 pacman -S adobe-source-han-sans-cn-fonts adobe-source-han-sans-tw-fonts adobe-source-han-sans-jp-fonts adobe-source-han-sans-kr-fonts

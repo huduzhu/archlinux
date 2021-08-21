7b84681d1c3c44626646c1e86aa606c041fd3621
huduzhu <88719966+huduzhu@users.noreply.github.com>
2021-08-21 22:06:28 +1000

    Update vmxfce.sh

--- vmxfce.sh
+++ vmxfce.sh
@@ -12,6 +12,6 @@ tar xf /mnt/VMwareTools*.tar.gz -C /root
 perl /root/vmware-tools-distrib/vmware-install.pl
 pacman -S virtualbox-guest-utils xf86-video-vmware
 systemctl enable vmwaretools.service  
-pacman -S xfce4 lightdm lightdm-gtk-greeter gvfs-smb xfce4-pulseaudio-plugin pulseaudio-alsa pavucontrol
+pacman -S xfce4 lightdm lightdm-gtk-greeter gvfs-smb xfce4-pulseaudio-plugin pulseaudio-alsa pavucontrol openssh
 sudo systemctl enable lightdm
 pacman -S adobe-source-han-sans-cn-fonts adobe-source-han-sans-tw-fonts adobe-source-han-sans-jp-fonts adobe-source-han-sans-kr-fonts

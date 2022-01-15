c41a84f6ceb0ffb937faf3ed8fe7959f04990649
huduzhu <88719966+huduzhu@users.noreply.github.com>
2022-01-15 18:45:34 +1100

    Update victorbasic.sh

--- victorbasic.sh
+++ victorbasic.sh
@@ -1,3 +1,3 @@
-sudo pacman -S fcitx-im fcitx-configtool fcitex-googlepinyin firefox vlc xbindkeys xdotool tigervnc
+sudo pacman -S fcitx-im fcitx-configtool fcitex-googlepinyin firefox vlc xbindkeys xdotool tigervnc gpicview
 nano .xbindkeysrc
 xbindkeys -d > .xbindkeysrc

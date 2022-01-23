f0c029dafd8e8167b9ee175e7b65678455885155
huduzhu <88719966+huduzhu@users.noreply.github.com>
2022-01-23 11:04:25 +1100

    Update vmxfce.sh

--- vmxfce.sh
+++ vmxfce.sh
@@ -21,6 +21,7 @@ pacman -S pavucontrol
 sudo pacman -S lxdm
 sudo systemctl enable lxdm
 nano /etc/lxdm/lxdm.conf
+#session=/usr/bin/startxfce4
 pacman -S adobe-source-han-sans-cn-fonts adobe-source-han-sans-tw-fonts adobe-source-han-sans-jp-fonts adobe-source-han-sans-kr-fonts
 pacman -S fcitx-googlepinyin fcitx-cloudpinyin
 #curl -OL https://raw.githubusercontent.com/huduzhu/archlinux/main/install.sh
